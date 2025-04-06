d6369e0bd13ae629945ae9b73e35db7bbee9a735
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-06 18:01:22 +0530

    implemented variables

---
+++ 04-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=$1
+person2=$2
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2:: Hello $person1 I am fine how are you doing ?"
+echo "$person1:: I am doing good $person2. what's going on?"
+echo "$person2:: I started learning shell scrit $person1"

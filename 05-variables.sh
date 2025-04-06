4c258d63be88d85c82180525e935bb25076c0888
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-06 18:11:57 +0530

    implemented variables

---
+++ 05-variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#USERNAME=Naga
+#PASSWORD=naga123
+
+echo "please enter your username::"
+
+read -s USERNAME #takes input into USERNAME variable
+
+echo "Username entered is: $Username"
+
+echo "please enter your password::"
+
+read -s PASSWORD
+
+echo "Password entered is : $Password"

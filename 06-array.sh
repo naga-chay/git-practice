ba14c53207e48dc34620fdd37ab0979b98988d4b
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-06 18:32:00 +0530

    implemented variables

--- 06-array.sh
+++ 06-array.sh
@@ -3,8 +3,8 @@
 # index starts from 0, size is 3 (3 elements)
 FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
-echo "First fruit is: $FRUITS[0]"
-echo "First fruit is: $FRUITS[1]"
-echo "First fruit is: $FRUITS[2]"
+echo "First fruit is: ${FRUITS[0]}"
+echo "First fruit is: ${FRUITS[1]}"
+echo "First fruit is: ${FRUITS[2]}"
 
-echo "First fruit is: $FRUITS[@]"
+echo "First fruit is: ${FRUITS[@]}"

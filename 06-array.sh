8428d22c277c7e4d6a36dc3e9c31e9d0ea6733b5
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-06 18:33:11 +0530

    implemented variables

--- 06-array.sh
+++ 06-array.sh
@@ -4,7 +4,7 @@
 FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
 echo "First fruit is: ${FRUITS[0]}"
-echo "First fruit is: ${FRUITS[1]}"
-echo "First fruit is: ${FRUITS[2]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
 
-echo "First fruit is: ${FRUITS[@]}"
+echo "ALL fruits are: ${FRUITS[@]}"

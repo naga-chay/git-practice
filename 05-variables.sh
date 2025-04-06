2de0c31196a7260a6423fd8c10773dbbaa11bda1
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-06 18:15:23 +0530

    implemented variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,14 +3,14 @@
 #USERNAME=Naga
 #PASSWORD=naga123
 
-echo "please enter your username::"
+echo "Please enter your username::"
 
 read -s USERNAME #takes input into USERNAME variable
 
-echo "Username entered is: $username"
+echo "Username entered is: $USERNAME"
 
 echo "please enter your password::"
 
 read -s PASSWORD
 
-echo "Password entered is : $password"
+echo "Password entered is : $PASSWORD"

843bdd7ffbe83cb9a64142374cfd7a09db92eefd
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-13 00:56:07 +0530

    loops

--- 12-colors.sh
+++ 12-colors.sh
@@ -12,13 +12,13 @@ echo "Script started executing at:$TIMESTAMP"
 
 VALIDATE(){
 
-   if [ $1 -ne 0 ]
-   then 
-       echo -e "$2...$R FAILURE $N"   
-       exit 1
-   else
-       echo -e "$2... $G SUCCESS $N"
-   fi   
+    if [ $1 -ne 0 ]
+    then 
+         echo -e "$2...$R FAILURE $N"   
+         exit 1
+    else
+        echo -e "$2... $G SUCCESS $N"
+    fi   
 }
 
    if [ $USERID -ne 0 ]

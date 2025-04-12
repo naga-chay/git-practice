02f368301b3e20692d3c7b722b76d2aa200e691b
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-13 01:46:15 +0530

    installion

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -22,23 +22,22 @@ VALIDATE(){
 
 if [ $USERID -ne 0 ]
    then 
-       echo "Please run this script with root access."
-       exit 1 # manually exit if error comes.
+         echo "Please run this script with root access."
+        exit 1 # manually exit if error comes.
    else
-       echo "you are super user."
+         echo "you are super user."
    fi
 
   for i in $@
   do
-      echo "packages to install: $i"
-      dnf list installed $i &>>$LOGFILE
-     
- if  [$? -eq 0]
+         echo "packages to install: $i"
+         dnf list installed $i &>>$LOGFILE
+      if  [$? -eq 0]
       then 
-          echo -e "$i already installed....$Y skipping $N"
+         echo -e "$i already installed....$Y skipping $N"
       else
-          dnf install $i -y &>>$LOGFILE
-          VALIDATE $? "Installaton of $i"
+         dnf install $i -y &>>$LOGFILE
+         VALIDATE $? "Installaton of $i"
       fi    
   done
 

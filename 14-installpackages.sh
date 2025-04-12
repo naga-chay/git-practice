c545212b7231113e41ee4f362b175767137434bf
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-13 01:42:46 +0530

    installion

--- 14-installpackages.sh
+++ 14-installpackages.sh
@@ -33,7 +33,7 @@ if [ $USERID -ne 0 ]
       echo "packages to install: $i"
       dnf list installed $i &>>$LOGFILE
      
- if  [$? -ne 0]
+ if  [$? -eq 0]
       then 
           echo -e "$i already installed....$Y skipping $N"
       else

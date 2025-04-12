7de2201b4dce04b3086c03518e9434d9c54dc67a
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-12 18:39:24 +0530

    functions

--- 11-function.sh
+++ 11-function.sh
@@ -3,22 +3,22 @@
 USERID=$(id -u)
 
 VALIDATE(){
- if [$1 is -ne 0]
- then 
+  if [$1 is -ne 0]
+  then 
     echo "$2...FAILURE"   
     exit 1
- else
+  else
     echo "$2...SUCCESS"
- fi       
+  fi       
 }
 
- if [$USERID -ne 0]
- then 
+  if [$USERID -ne 0]
+  then 
     echo "Please run this script with root access."
     exit 1 # manually exit if error comes.
- else
+  else
     echo "you are super user."
- fi
+  fi
 
 dnf install mysql -y
 VALIDATE $? "installing MySql"

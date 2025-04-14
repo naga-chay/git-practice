c38e7a2daa2fa12d43b16925790747c41032870d
naga-chay <ballalanagachaitanya@gmail.com>
2025-04-15 00:54:00 +0530

    set

---
+++ 17-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+failure(){
+    echo "Failed at $1: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+
+if [$USERID -ne 0]
+then 
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
+else
+    echo "you are super user."
+fi
+
+dnf install mysql -y
+dnf install git -y
+
+echo "is script proceeding?"
